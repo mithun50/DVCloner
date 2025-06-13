14288c0a61831eaf80ad2a33e45857192e3efc76
Mithun Gowda B <mithungowda.b7411@gmail.com>
2025-06-13 18:13:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,4 +71,4 @@ def synthesize():
         return jsonify({"success": False, "error": str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
